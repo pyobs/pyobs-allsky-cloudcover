Fixed coverage calculation

pyobs_cloudcover/pipeline/night/cloud_coverage_calculator/coverage_calculator.py
@@ -9,7 +9,7 @@ class CoverageCalculator(object):
     def __call__(self, cloud_map: npt.NDArray[np.float_]) -> float:
         cloud_map_values = cloud_map[~np.isnan(cloud_map)]
 
-        clouds = np.sum((cloud_map_values >= self._threshold).astype(np.uint8))
+        clouds = np.sum((cloud_map_values <= self._threshold).astype(np.uint8))
         free_space = np.sum((cloud_map_values >= 0.0).astype(np.uint8))
 
         return float(clouds / free_space)

pyobs_cloudcover/pipeline/night/cloud_coverage_calculator/coverage_info_calculator.py
@@ -22,6 +22,9 @@ class CoverageInfoCalculator:
     def __call__(self, cloud_map: npt.NDArray[np.float_], obs_time: datetime.datetime, alt_az_list: List[List[Optional[AltAzCoord]]]) -> CloudCoverageInfo:
         change = self._coverage_change_calculator(cloud_map)
         coverage = self._coverage_calculator(cloud_map)
-        zenith_coverage = self._coverage_calculator(self._zenith_masker(cloud_map, alt_az_list))
 
-        return CloudCoverageInfo(copy(cloud_map), coverage, zenith_coverage, change, obs_time)
+        zenith = self._zenith_masker(cloud_map, alt_az_list)
+        zenith_coverage = self._coverage_calculator(zenith)
+        zenith_average = float(np.mean(zenith, where=~np.isnan(zenith)))
+
+        return CloudCoverageInfo(copy(cloud_map), coverage, zenith_coverage, zenith_average, change, obs_time)

pyobs_cloudcover/pipeline/night/star_reverse_matcher/detector/sigma_treshhold_detector.py
@@ -12,10 +12,17 @@ class SigmaThresholdDetector(StarDetector):
 
     def __call__(self, image: npt.NDArray[np.float_]) -> bool:
         median = np.median(image)
-        average = np.average(image)
-        std = np.std(image)
 
         if median > self._median_limit:
             return False
 
-        return True in (image > average + self._sigma * std)
+        average = np.average(image)
+        std = np.std(image)
+
+        nx, ny = image.shape
+        center_x, center_y = (nx//2, ny//2)
+        x, y = np.arange(0, nx), np.arange(0, ny)
+        x_grid, y_grid = np.meshgrid(x, y)
+        circ_mask = (x_grid - center_x)**2 + (y_grid - center_y)**2 < self._distance**2
+
+        return True in (circ_mask.transpose() * image > average + self._sigma * std)

tests/unit/pipeline/night/cloud_coverage_calculator/test_zenith_mask.py
@@ -26,9 +26,9 @@ def test_zenith_mask() -> None:
     image = np.ones((3, 3))
 
     alt_az_list: List[List[Optional[AltAzCoord]]] = [
-        [AltAzCoord(70, 0), AltAzCoord(85, 0), AltAzCoord(70, 0)],
-        [AltAzCoord(85, 0), AltAzCoord(85, 0), AltAzCoord(85, 0)],
-        [AltAzCoord(70, 0), AltAzCoord(85, 0), AltAzCoord(70, 0)]
+        [AltAzCoord(np.deg2rad(70), 0), AltAzCoord(np.deg2rad(85), 0), AltAzCoord(np.deg2rad(70), 0)],
+        [AltAzCoord(np.deg2rad(85), 0), AltAzCoord(np.deg2rad(85), 0), AltAzCoord(np.deg2rad(85), 0)],
+        [AltAzCoord(np.deg2rad(70), 0), AltAzCoord(np.deg2rad(85), 0), AltAzCoord(np.deg2rad(70), 0)],
     ]
 
     masked_image = masker(image, alt_az_list)

tests/unit/pipeline/test_pipeline_controller.py
@@ -17,7 +17,7 @@ class MockPipeline(Pipeline):
 
     def __call__(self, image: npt.NDArray[np.float_], obs_time: datetime.datetime) -> CloudCoverageInfo:
         time = datetime.datetime(2024, 1, 1, 0, 0, 0)
-        return CloudCoverageInfo(np.array([]), 0, 0, 0, time)
+        return CloudCoverageInfo(np.array([]), 0, 0, 0, 0, time)
 
 
 @pytest.fixture()

tests/unit/web_api/test_coverage_query_executor.py
@@ -44,7 +44,7 @@ def test_call_map(observer, obs_time, model):
     executor._radec_to_altaz = Mock(return_value=(np.pi/2, 0))
     executor._model.altaz_to_pix = Mock(return_value=(1, 1))
 
-    executor.set_measurement(CloudCoverageInfo(np.identity(3), 0, 0, 0, obs_time))
+    executor.set_measurement(CloudCoverageInfo(np.identity(3), 0, 0, 0, 0, obs_time))
 
     assert executor(10, 10) == 1/3
     executor._radec_to_altaz.assert_called_once_with(10, 10, obs_time)
@@ -58,7 +58,7 @@ def test_call_map_out_of_bounds(observer, obs_time, model):
     executor._radec_to_altaz = Mock(return_value=(np.pi/2, 0))
     executor._model.altaz_to_pix = Mock(return_value=(5, 5))
 
-    executor.set_measurement(CloudCoverageInfo(np.identity(3), 0, 0, 0, obs_time))
+    executor.set_measurement(CloudCoverageInfo(np.identity(3), 0, 0, 0, 0, obs_time))
 
     assert executor(10, 10) is None
 
@@ -72,6 +72,6 @@ def test_call_map_nan(observer, obs_time, model):
 
     cloud_map = np.identity(3)
     cloud_map[0, 0] = np.nan
-    executor.set_measurement(CloudCoverageInfo(cloud_map, 0, 0, 0, obs_time))
+    executor.set_measurement(CloudCoverageInfo(cloud_map, 0, 0, 0, 0, obs_time))
 
     assert executor(10, 10) == 2/8