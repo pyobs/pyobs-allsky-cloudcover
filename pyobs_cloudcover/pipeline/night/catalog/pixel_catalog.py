019ae3e75fa350834530c79256bffac18348bd0e
germanhydrogen <admin@germanhydrogen.org>
2024-06-17 11:26:55 +0200

    Fixed filter close

--- pyobs_cloudcover/pipeline/night/catalog/pixel_catalog.py
+++ pyobs_cloudcover/pipeline/night/catalog/pixel_catalog.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import datetime
-from typing import cast
+from typing import cast, List, Tuple, Set
 
 import numpy as np
 import numpy.typing as npt
@@ -29,17 +29,53 @@ class PixelCatalog(object):
         self.v_mag = self.v_mag[condition]
 
     def filter_close(self, distance: float) -> None:
-        neighbourhood = np.array(
-            [np.sum(self._in_neighbourhood(x, y, self.px, self.py, distance)) for x, y in zip(self.px, self.py)
-             ]) - 1
+        indices = np.arange(len(self.sao))
+        neighbourhood = [
+                indices[self._in_neighbourhood(x, y, self.px, self.py, distance)] for x, y in zip(self.px, self.py)
+             ]
 
-        self._filter(neighbourhood == 0)
+        neighbors = list(map(tuple, (filter(lambda x: len(x) > 1, neighbourhood))))
+
+        if len(neighbors) == 0:
+            return
+
+        clusters = self._find_cluster(neighbors)
+
+        tuple_indices_to_delete = [self._get_faintest_indices(group) for group in clusters]
+
+        stars_to_delete = [entry for group, delete in zip(clusters, tuple_indices_to_delete) for entry in group[delete]]
+
+        indices_to_delete = np.isin(indices, stars_to_delete)
+        self._filter(~indices_to_delete)
 
     @staticmethod
     def _in_neighbourhood(x: float, y: float, px: npt.NDArray[np.float_], py: npt.NDArray[np.float_], d: float) -> npt.NDArray[np.bool_]:
-        inside_x_axis: npt.NDArray[np.bool_] = (np.abs(x - px) <= d)
-        inside_y_axis: npt.NDArray[np.bool_] = (np.abs(y - py) <= d)
-        return inside_x_axis & inside_y_axis
+        in_neighbourhood: npt.NDArray[np.bool_] = np.sqrt(np.square(x - px) + np.square(y - py)) <= d
+        return in_neighbourhood
+
+    @staticmethod
+    def _find_cluster(neighbours: List[Tuple[np.int_, ...]]) -> List[npt.NDArray[np.int_]]:
+        """
+        Finds clusters in list of neighbours
+        """
+
+        unique_neighbour_pairs = list(set(neighbours))
+        clusters: List[Set[np.int_]] = [set(unique_neighbour_pairs[0])]
+
+        for pair in unique_neighbour_pairs[1:]:
+            for group in clusters:
+                if any([x in group for x in pair]):
+                    group.update(set(pair))
+                    break
+            else:
+                clusters.append(set(pair))
+
+        cluster_list: List[npt.NDArray[np.int_]] = list(map(lambda x: np.array(list(x)), clusters))
+
+        return cluster_list
+
+    def _get_faintest_indices(self, cluster: npt.NDArray[np.int_]) -> npt.NDArray[np.int_]:
+        return np.argsort(self.v_mag[cluster])[1:]
 
     def filter_window_size(self, height: int, width: int) -> None:
         inside_x_axis = (0 < self.px) & (self.px < width)
--- tests/unit/pipeline/night/catalog/test_pixel_catalog.py
+++ tests/unit/pipeline/night/catalog/test_pixel_catalog.py
@@ -9,14 +9,22 @@ def test_filter_close() -> None:
     az = np.array([0, 0, 180])
     px = np.array([10, 10, 20])
     py = np.array([10, 15, 20])
-    v_mag = np.array([0, 0, 0])
+    v_mag = np.array([1, 0, 0])
 
     catalog = PixelCatalog(sao, alt, az, px, py, v_mag)
     catalog.filter_close(5)
 
-    np.testing.assert_array_equal(catalog.sao, sao[2:])
-    np.testing.assert_array_equal(catalog.alt, alt[2:])
-    np.testing.assert_array_equal(catalog.az, az[2:])
-    np.testing.assert_array_equal(catalog.px, px[2:])
-    np.testing.assert_array_equal(catalog.py, py[2:])
-    np.testing.assert_array_equal(catalog.v_mag, v_mag[2:])
+    np.testing.assert_array_equal(catalog.sao, sao[1:])
+    np.testing.assert_array_equal(catalog.alt, alt[1:])
+    np.testing.assert_array_equal(catalog.az, az[1:])
+    np.testing.assert_array_equal(catalog.px, px[1:])
+    np.testing.assert_array_equal(catalog.py, py[1:])
+    np.testing.assert_array_equal(catalog.v_mag, v_mag[1:])
+
+
+def test_find_cluster() -> None:
+    neighbours = [(1, 2), (6, 7), (2, 4, 5), (1, 2)]
+
+    clusters = PixelCatalog._find_cluster(neighbours)  # type: ignore
+
+    assert set(map(lambda x: tuple(x), clusters)) == {(1, 2, 4, 5), (6, 7)}
