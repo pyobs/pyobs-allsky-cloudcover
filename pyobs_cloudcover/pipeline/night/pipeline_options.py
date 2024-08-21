a0a619b2128634c7fb8a1a39f0267ee1a4453ca9
germanhydrogen <admin@germanhydrogen.org>
2024-08-21 14:40:25 +0200

    Fixed typing

--- pyobs_cloudcover/pipeline/night/pipeline_options.py
+++ pyobs_cloudcover/pipeline/night/pipeline_options.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Dict, Any
+from typing import Dict, Any, cast
 
 from pyobs_cloudcover.pipeline.night.altaz_grid_generator.altaz_grid_options import AltAzGridOptions
 from pyobs_cloudcover.pipeline.night.catalog.catalog_constructor_options import CatalogConstructorOptions
@@ -41,7 +41,7 @@ class NightPipelineOptions(object):
         lim_mag_map_generator_options = LimMagnitudeMapGeneratorOptions.from_dict(options.get("lim_mag_map", {}))
         cloud_map_generator_options = CloudMapGeneratorOptions.from_dict(options.get("cloud_map", {}))
         altaz_grid_generator = AltAzGridOptions.from_dict(options.get("altaz_grid", {}))
-        moon_apparent_size = options.get("moon_apparent_size", 10)
+        moon_apparent_size = cast(float, options.get("moon_apparent_size", 10.0))
 
         return cls(model_options,
                    preprocessor_options,
